29d5cae8bf26d6d69e8e1f3bdd41fd26eeca05a3
priyaganti <ganti.priya@gmail.com>
2017-06-02 14:55:20 -0700

    add gulp watch task to watch for changes in the files and transpile them

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,10 +7,19 @@ import gulp from "gulp";
 import browserify from "browserify";
 import source from "vinyl-source-stream";
 
-gulp.task("default", () => {
+gulp.task("transpile", () => {
   return browserify("src/app.js")
   .transform("babelify")
   .bundle()
   .pipe(source("bundle.js"))
   .pipe(gulp.dest("dist"));
 });
+
+// add gulp task to watch for changes and transpile the code without manually entering gulp command after a change
+
+//watch Any file under the src folder ending with .js extension and every time any of these files change run the transpile task
+gulp.task("watch", ["transpile"], () => {
+  gulp.watch("src/**/*.js", ["transpile"]);
+});
+
+gulp.task("default",["transpile"]); //this indicates that transpile is a dependency before running the default task.
