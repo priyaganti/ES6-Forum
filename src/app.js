68dd67e6d53caea33344239c076a326a1ba61405
priyaganti <ganti.priya@gmail.com>
2017-06-03 07:57:21 -0700

    create seperate modules for post and ui objects and import them in app.js

--- src/app.js
+++ src/app.js
@@ -1,27 +1,7 @@
-/* Create Post object with findAll method */
-let Post = {
-  findAll(){
-    return new Promise( (resolve, reject) => {
-      let uri = "http://localhost:3000/posts";
-      let request = new XMLHttpRequest();
-      request.open("GET", uri, true);
-      request.onload = () => {
-        if(request.status >= 200 && request.status < 400){
-          resolve(request.response);
-        }
-      };
-      request.onerror = () => {
-        reject(new Error("Something Went wrong on the API"));
-      }
-      request.send();
-    });
-  }
-}
+import Post from './post.js';
+import ui from './ui.js';
 
-let ui = {
-  renderPosts(posts){
-    console.log(posts);
-  }
-}
 
-Post.findAll().then(ui.renderPosts).catch( (error) => console.log(error));
+Post.findAll()
+.then(ui.renderPosts)
+.catch( (error) => console.log(error));
